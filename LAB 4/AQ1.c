d6ba10ef4989b853f0f35da99cecc9e3732eb047
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2024-08-13 10:51:56 +0530

    Update AQ1.c

--- LAB 4/AQ1.c
+++ LAB 4/AQ1.c
@@ -8,24 +8,22 @@ typedef struct {
     int value;
 } Element;
 
-void transpose(Element a[], Element b[]) {
-    int num = a[0].value;
-    int Cols = a[0].col;
+void transposeSparseMatrix(Element a[], Element b[]) {
+    int numTerms = a[0].value;
+    int numCols = a[0].col;
 
-    b[0].row = Cols;
+    b[0].row = numCols;
     b[0].col = a[0].row;
-    b[0].value = num;
+    b[0].value = numTerms;
 
-    if (num > 0) {
-        int B = 1;
-        for (int i = 0; i < Cols; i++) {
-            for (int j = 1; j <= num; j++) {
-                if (a[j].col == i) {
-                    b[B].row = a[j].col;
-                    b[B].col = a[j].row;
-                    b[B].value = a[j].value;
-                    B++;
-                }
+    int k = 1;
+    for (int i = 0; i < numCols; i++) {
+        for (int j = 1; j <= numTerms; j++) {
+            if (a[j].col == i) {
+                b[k].row = a[j].col;
+                b[k].col = a[j].row;
+                b[k].value = a[j].value;
+                k++;
             }
         }
     }
@@ -33,27 +31,67 @@ void transpose(Element a[], Element b[]) {
 
 int main() {
     Element a[MAX], b[MAX];
-    int num;
+    int num, totalElements;
 
     printf("Enter the number of rows and columns of the sparse matrix: ");
     scanf("%d %d", &a[0].row, &a[0].col);
 
+    totalElements = a[0].row * a[0].col;
+
     printf("Enter the number of non-zero elements: ");
     scanf("%d", &num);
     a[0].value = num;
+
+    if (num > totalElements / 2) {
+        printf("Not a sparse matrix\n");
+        return 0;
+    }
 
     printf("Enter the row, column, and value of each non-zero element:\n");
     for (int i = 1; i <= num; i++) {
         scanf("%d %d %d", &a[i].row, &a[i].col, &a[i].value);
     }
 
-    transpose(a, b);
+
+    printf("The original matrix is:\n");
+    for (int i = 0; i < a[0].row; i++) {
+        for (int j = 0; j < a[0].col; j++) {
+            int found = 0;
+            for (int k = 1; k <= num; k++) {
+                if (a[k].row == i && a[k].col == j) {
+                    printf("%d ", a[k].value);
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found) {
+                printf("0 ");
+            }
+        }
+        printf("\n");
+    }
+
+
+    transposeSparseMatrix(a, b);
+
 
     printf("The transposed matrix is:\n");
-    for (int i = 0; i <= b[0].value; i++) {
-        printf("%d %d %d\n", b[i].row, b[i].col, b[i].value);
+    for (int i = 0; i < b[0].row; i++) {
+        for (int j = 0; j < b[0].col; j++) {
+            int found = 0;
+            for (int k = 1; k <= b[0].value; k++) {
+                if (b[k].row == i && b[k].col == j) {
+                    printf("%d ", b[k].value);
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found) {
+                printf("0 ");
+            }
+        }
+        printf("\n");
     }
 
     return 0;
 }
-
