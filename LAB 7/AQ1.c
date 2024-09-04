37c5e5f2aa2e1a123c2d7e4c6c173416501f15c7
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2024-09-04 15:47:32 +0530

    Create AQ1.c

---
+++ LAB 7/AQ1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* create_list(int data[], int size) {
+    if (size == 0) {
+        return NULL;
+    } else {
+        struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+        head->data = data[0];
+        head->next = create_list(data + 1, size - 1);
+        return head;
+    }
+}
+
+void print_list(struct Node* head) {
+    if (head == NULL) {
+        return;
+    } else {
+        printf("%d ", head->data);
+        print_list(head->next);
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int data[n];
+    printf("Enter the elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &data[i]);
+    }
+
+    struct Node* head = create_list(data, n);
+    printf("Traversing the list:\n");
+    print_list(head);
+
+    return 0;
+}
