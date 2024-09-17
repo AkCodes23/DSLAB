b32b2b609897ec760e7a20d93e758ae15afeb854
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2024-09-17 10:43:22 +0530

    Create Q2.c

---
+++ LAB 9/Q2.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int coeff;
+    int pow;
+    struct Node* next;
+};
+
+void create_node(int coeff, int pow, struct Node** temp) {
+    struct Node* r, * z;
+    z = *temp;
+    if (z == NULL) {
+        r = (struct Node*)malloc(sizeof(struct Node));
+        r->coeff = coeff;
+        r->pow = pow;
+        *temp = r;
+        r->next = (struct Node*)malloc(sizeof(struct Node));
+        r = r->next;
+        r->next = NULL;
+    } else {
+        r->coeff = coeff;
+        r->pow = pow;
+        r->next = (struct Node*)malloc(sizeof(struct Node));
+        r = r->next;
+        r->next = NULL;
+    }
+}
+
+void polyadd(struct Node* poly1, struct Node* poly2, struct Node* poly) {
+    while (poly1->next && poly2->next) {
+        if (poly1->pow > poly2->pow) {
+            poly->pow = poly1->pow;
+            poly->coeff = poly1->coeff;
+            poly1 = poly1->next;
+        } else if (poly1->pow < poly2->pow) {
+            poly->pow = poly2->pow;
+            poly->coeff = poly2->coeff;
+            poly2 = poly2->next;
+        } else {
+            poly->pow = poly1->pow;
+            poly->coeff = poly1->coeff + poly2->coeff;
+            poly1 = poly1->next;
+            poly2 = poly2->next;
+        }
+        poly->next = (struct Node*)malloc(sizeof(struct Node));
+        poly = poly->next;
+        poly->next = NULL;
+    }
+    while (poly1->next || poly2->next) {
+        if (poly1->next) {
+            poly->pow = poly1->pow;
+            poly->coeff = poly1->coeff;
+            poly1 = poly1->next;
+        }
+        if (poly2->next) {
+            poly->pow = poly2->pow;
+            poly->coeff = poly2->coeff;
+            poly2 = poly2->next;
+        }
+        poly->next = (struct Node*)malloc(sizeof(struct Node));
+        poly = poly->next;
+        poly->next = NULL;
+    }
+}
+
+void show(struct Node* node) {
+    while (node->next != NULL) {
+        printf("%dx^%d", node->coeff, node->pow);
+        node = node->next;
+        if (node->coeff >= 0) {
+            if (node->next != NULL)
+                printf("+");
+        }
+    }
+}
+
+int main() {
+    struct Node *poly1 = NULL, *poly2 = NULL, *poly = NULL;
+    int n, coeff, pow;
+
+    printf("Enter number of terms for 1st polynomial: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        printf("Enter coefficient and power: ");
+        scanf("%d %d", &coeff, &pow);
+        create_node(coeff, pow, &poly1);
+    }
+
+    printf("Enter number of terms for 2nd polynomial: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        printf("Enter coefficient and power: ");
+        scanf("%d %d", &coeff, &pow);
+        create_node(coeff, pow, &poly2);
+    }
+
+    poly = (struct Node*)malloc(sizeof(struct Node));
+    polyadd(poly1, poly2, poly);
+
+    printf("Added polynomial: ");
+    show(poly);
+
+    return 0;
+}
