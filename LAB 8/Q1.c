db9b88001352b8aac609943c7fe640174121b364
Akhil Varanasi <135016848+AkCodes23@users.noreply.github.com>
2024-09-17 10:21:25 +0530

    Update Q1.c

--- LAB 8/Q1.c
+++ LAB 8/Q1.c
@@ -1,248 +1,275 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
-    int data;
-    struct Node* prev;
-    struct Node* next;
-};
+#define MAX 100
 
-struct Node* head = NULL;
+int data[MAX];
+int next[MAX];
+int prev[MAX];
+int head = -1;
+int tail = -1;
+int freeIndex = 0;
 
-void insertAtRear(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->prev = NULL;
-    newNode->next = NULL;
-
-    if (head == NULL) {
-        head = newNode;
-    } else {
-        struct Node* temp = head;
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        temp->next = newNode;
-        newNode->prev = temp;
-    }
-}
-
-void deleteAtRear() {
-    if (head == NULL) {
-        return;
-    }
-
-    struct Node* temp = head;
-    while (temp->next != NULL) {
-        temp = temp->next;
-    }
-
-    if (temp == head) {
-        head = NULL;
-    } else {
-        temp->prev->next = NULL;
-    }
-    free(temp);
-}
-
-void insertAtPosition(int data, int position) {
-    if (position < 1) {
-        printf("Invalid position.\n");
-        return;
-    }
-
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-
-    if (position == 1) {
-        newNode->next = head;
-        if (head != NULL) {
-            head->prev = newNode;
-        }
-        head = newNode;
-    } else {
-        struct Node* temp = head;
-        for (int i = 1; i < position - 1 && temp != NULL; i++) {
-            temp = temp->next;
-        }
-
-        if (temp == NULL) {
-            printf("Invalid position.\n");
-            free(newNode);
-            return;
-        }
-
-        newNode->next = temp->next;
-        if (temp->next != NULL) {
-            temp->next->prev = newNode;
-        }
-        temp->next = newNode;
-        newNode->prev = temp;
-    }
-}
-
-void deleteAtPosition(int position) {
-    if (position < 1) {
-        printf("Invalid position.\n");
-        return;
-    }
-
-    if (position == 1) {
-        struct Node* temp = head;
-        head = head->next;
-        if (head != NULL) {
-            head->prev = NULL;
-        }
-        free(temp);
-    } else {
-        struct Node* temp = head;
-        for (int i = 1; i < position - 1 && temp != NULL; i++) {
-            temp = temp->next;
-        }
-
-        if (temp == NULL || temp->next == NULL) {
-            printf("Invalid position.\n");
-            return;
-        }
-
-        struct Node* toDelete = temp->next;
-        temp->next = toDelete->next;
-        if (toDelete->next != NULL) {
-            toDelete->next->prev = temp;
-        }
-        free(toDelete);
-    }
-}
-
-void insertAfter(int data, int afterData) {
-    struct Node* temp = head;
-    while (temp != NULL && temp->data != afterData) {
-        temp = temp->next;
-    }
-
-    if (temp == NULL) {
-        printf("Element not found.\n");
-        return;
-    }
-
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->next = temp->next;
-    newNode->prev = temp;
-
-    if (temp->next != NULL) {
-        temp->next->prev = newNode;
-    }
-    temp->next = newNode;
-}
-
-void insertBefore(int data, int beforeData) {
-    struct Node* temp = head;
-    while (temp != NULL && temp->data != beforeData) {
-        temp = temp->next;
-    }
-
-    if (temp == NULL) {
-        printf("Element not found.\n");
-        return;
-    }
-
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->next = temp;
-    newNode->prev = temp->prev;
-
-    if (temp->prev != NULL) {
-        temp->prev->next = newNode;
-    }
-    temp->prev = newNode;
-}
-
-void traverse() {
-    struct Node* temp = head;
-    while (temp != NULL) {
-        printf("%d ", temp->data);
-        temp = temp->next;
-    }
-    printf("\n");
-}
-
-void reverse() {
-    if (head == NULL || head->next == NULL) {
-        return;
-    }
-
-    struct Node* current = head;
-    struct Node* next = NULL;
-    struct Node* prev = NULL;
-
-    while (current != NULL) {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
-
-    head = prev;
-}
+void insertAtEnd(int value);
+void deleteFromEnd();
+void insertAtPosition(int value, int position);
+void deleteFromPosition(int position);
+void insertAfterElement(int value, int element);
+void insertBeforeElement(int value, int element);
+void traverseList();
+void reverseList();
 
 int main() {
-    int choice, data, position;
+    int choice, value, position, element;
 
     while (1) {
-        printf("1. Insert at rear\n");
-        printf("2. Delete at rear\n");
+        printf("\nMenu:\n");
+        printf("1. Insert at end\n");
+        printf("2. Delete from end\n");
         printf("3. Insert at position\n");
-        printf("4. Delete at position\n");
-        printf("5. Insert after\n");
-        printf("6. Insert before\n");
-        printf("7. Traverse\n");
-        printf("8. Reverse\n");
+        printf("4. Delete from position\n");
+        printf("5. Insert after element\n");
+        printf("6. Insert before element\n");
+        printf("7. Traverse list\n");
+        printf("8. Reverse list\n");
         printf("9. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-                printf("Enter data: ");
-                scanf("%d", &data);
-                insertAtRear(data);
+                printf("Enter data to insert at end: ");
+                scanf("%d", &value);
+                insertAtEnd(value);
                 break;
             case 2:
-                deleteAtRear();
+                deleteFromEnd();
                 break;
             case 3:
-                printf("Enter data and position: ");
-                scanf("%d %d", &data, &position);
-                insertAtPosition(data, position);
+                printf("Enter position to insert at: ");
+                scanf("%d", &position);
+                printf("Enter data to insert: ");
+                scanf("%d", &value);
+                insertAtPosition(value, position);
                 break;
             case 4:
-                printf("Enter position: ");
+                printf("Enter position to delete from: ");
                 scanf("%d", &position);
-                deleteAtPosition(position);
+                deleteFromPosition(position);
                 break;
             case 5:
-                printf("Enter data and after which element: ");
-                scanf("%d %d", &data, &afterData);
-                insertAfter(data, afterData);
+                printf("Enter element to insert after: ");
+                scanf("%d", &element);
+                printf("Enter data to insert: ");
+                scanf("%d", &value);
+                insertAfterElement(value, element);
                 break;
             case 6:
-                printf("Enter data and before which element: ");
-                scanf("%d %d", &data, &beforeData);
-                insertBefore(data, beforeData);
+                printf("Enter element to insert before: ");
+                scanf("%d", &element);
+                printf("Enter data to insert: ");
+                scanf("%d", &value);
+                insertBeforeElement(value, element);
                 break;
             case 7:
-                traverse();
+                traverseList();
                 break;
             case 8:
-                reverse();
+                reverseList();
                 break;
             case 9:
                 exit(0);
             default:
-                printf("Invalid choice.\n");
+                printf("Invalid choice! Please try again.\n");
         }
     }
 
     return 0;
+}
+
+void insertAtEnd(int value) {
+    if (freeIndex >= MAX) {
+        printf("List is full!\n");
+        return;
+    }
+
+    data[freeIndex] = value;
+    next[freeIndex] = -1;
+    prev[freeIndex] = tail;
+
+    if (tail != -1) {
+        next[tail] = freeIndex;
+    } else {
+        head = freeIndex;
+    }
+
+    tail = freeIndex;
+    freeIndex++;
+}
+
+void deleteFromEnd() {
+    if (tail == -1) {
+        printf("List is empty!\n");
+        return;
+    }
+
+    int temp = tail;
+    tail = prev[tail];
+
+    if (tail != -1) {
+        next[tail] = -1;
+    } else {
+        head = -1;
+    }
+
+    freeIndex--;
+}
+
+void insertAtPosition(int value, int position) {
+    if (freeIndex >= MAX) {
+        printf("List is full!\n");
+        return;
+    }
+
+    if (position < 1 || position > freeIndex + 1) {
+        printf("Position out of range!\n");
+        return;
+    }
+
+    if (position == freeIndex + 1) {
+        insertAtEnd(value);
+        return;
+    }
+
+    int newIndex = freeIndex;
+    data[newIndex] = value;
+
+    if (position == 1) {
+        next[newIndex] = head;
+        prev[newIndex] = -1;
+        prev[head] = newIndex;
+        head = newIndex;
+    } else {
+        int temp = head;
+        for (int i = 1; i < position - 1; i++) {
+            temp = next[temp];
+        }
+
+        next[newIndex] = next[temp];
+        prev[newIndex] = temp;
+        prev[next[temp]] = newIndex;
+        next[temp] = newIndex;
+    }
+
+    freeIndex++;
+}
+
+void deleteFromPosition(int position) {
+    if (position < 1 || position > freeIndex) {
+        printf("Position out of range!\n");
+        return;
+    }
+
+    if (position == freeIndex) {
+        deleteFromEnd();
+        return;
+    }
+
+    int temp = head;
+    for (int i = 1; i < position; i++) {
+        temp = next[temp];
+    }
+
+    if (prev[temp] != -1) {
+        next[prev[temp]] = next[temp];
+    } else {
+        head = next[temp];
+    }
+
+    if (next[temp] != -1) {
+        prev[next[temp]] = prev[temp];
+    } else {
+        tail = prev[temp];
+    }
+
+    freeIndex--;
+}
+
+void insertAfterElement(int value, int element) {
+    int temp = head;
+    while (temp != -1 && data[temp] != element) {
+        temp = next[temp];
+    }
+
+    if (temp == -1) {
+        printf("Element not found!\n");
+        return;
+    }
+
+    int newIndex = freeIndex;
+    data[newIndex] = value;
+    next[newIndex] = next[temp];
+    prev[newIndex] = temp;
+
+    if (next[temp] != -1) {
+        prev[next[temp]] = newIndex;
+    } else {
+        tail = newIndex;
+    }
+
+    next[temp] = newIndex;
+    freeIndex++;
+}
+
+void insertBeforeElement(int value, int element) {
+    int temp = head;
+    while (temp != -1 && data[temp] != element) {
+        temp = next[temp];
+    }
+
+    if (temp == -1) {
+        printf("Element not found!\n");
+        return;
+    }
+
+    int newIndex = freeIndex;
+    data[newIndex] = value;
+    next[newIndex] = temp;
+    prev[newIndex] = prev[temp];
+
+    if (prev[temp] != -1) {
+        next[prev[temp]] = newIndex;
+    } else {
+        head = newIndex;
+    }
+
+    prev[temp] = newIndex;
+    freeIndex++;
+}
+
+void traverseList() {
+    int temp = head;
+    while (temp != -1) {
+        printf(" %d ", data[temp]);
+        temp = next[temp];
+    }
+    printf("\n");
+}
+
+void reverseList() {
+    int temp = head;
+    int prevTemp = -1;
+    int nextTemp;
+
+    while (temp != -1) {
+        nextTemp = next[temp];
+        next[temp] = prevTemp;
+        prev[temp] = nextTemp;
+        prevTemp = temp;
+        temp = nextTemp;
+    }
+
+    temp = head;
+    head = tail;
+    tail = temp;
 }
